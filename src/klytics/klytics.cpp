fe9c2e5d9c3cfab472d7408f70cbc65e1f5d0c30
logicp <adventurist@gmail.com>
2020-11-13 15:45:44 -0500

    formatting output adding expectation of third party tabulate library

--- src/klytics/klytics.cpp
+++ src/klytics/klytics.cpp
@@ -4,4 +4,16 @@ namespace klytics {
 ProcessResult execute(std::string program) {
   return qx({program}, get_executable_cwd());
 }
+
+void FormatTable(tabulate::Table& table) {
+using namespace tabulate;
+  for (int i = 0; i < 3; ++i) {
+    table[0][i].format()
+      .font_color(Color::blue)
+      .font_align(FontAlign::center)
+      .font_style({FontStyle::bold});
+  }
+  table.column(2).format()
+  .font_align(FontAlign::right);
+}
 } // namespace klytics
--- src/klytics/klytics.hpp
+++ src/klytics/klytics.hpp
@@ -5,6 +5,8 @@
 #include <ctime>
 #include <string>
 #include <process.hpp>
+#include <nlohmann/json.hpp>
+#include <tabulate/table.hpp>
 
 namespace klytics {
 namespace constants {
@@ -13,9 +15,97 @@ const std::string FOLLOWER_APP{"../external/followers/followers.js"};
 
 ProcessResult execute(std::string program);
 
+struct FollowerCount {
+std::string name;
+std::string platform;
+std::string value;
+};
+
+class ResultInterface {
+public:
+virtual std::string to_string() = 0;
+};
+
+/**
+ * FormatTable
+ *
+ * @helper function
+ */
+void FormatTable(tabulate::Table& table);
+
+class JSONResult : public ResultInterface {
+using counts = std::vector<FollowerCount>;
+public:
+virtual std::string to_string() override {
+  using namespace tabulate;
+
+  Table table{};
+  table.add_row({"Platform", "Name", "Count"});
+
+  for (const auto& result : m_counts) {
+    table.add_row({result.platform, result.name, result.value});
+  }
+
+  FormatTable(table);
+
+  return table.str();
+}
+
+/**
+ * read
+ */
+bool read(std::string s) {
+  using json = nlohmann::json;
+
+  json r_json = json::parse(s);
+
+  if (!r_json.is_null() && r_json.is_object()) {
+    auto instagram = r_json["instagram"];
+    auto youtube   = r_json["youtube"];
+
+    if (!instagram.is_null() && instagram.is_object()) {
+      for (const auto& it : instagram.items()) {
+        m_counts.push_back(
+          FollowerCount{
+            .name     = it.key(),
+            .platform = "instagram",
+            .value    = it.value()["value"].dump()
+          }
+        );
+      }
+    }
+
+    if (!youtube.is_null() && youtube.is_object()) {
+      for (const auto& it : youtube.items()) {
+      m_counts.push_back(
+        FollowerCount{
+          .name     = it.key(),
+          .platform = "youtube",
+          .value    = it.value()["value"].dump()
+        }
+      );
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+private:
+counts m_counts;
+};
+
+/**
+ *
+ * KLytics - Main class
+ *
+ */
 class KLytics {
 public:
 
+/**
+ * get_follower_count
+ */
 std::string get_follower_count() {
   ProcessResult result = execute(constants::FOLLOWER_APP);
 
@@ -23,7 +113,13 @@ std::string get_follower_count() {
     return "Error executing followers app";
   }
 
-  return result.output;
+  JSONResult json{};
+
+  if (json.read(result.output)) {
+    return json.to_string();
+  }
+
+  return "Error processing followers app data";
 }
 };
 
