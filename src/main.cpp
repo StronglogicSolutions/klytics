778ec94d57df2666ba9770f0231897dfc229d9b2
logicp <adventurist@gmail.com>
2020-11-13 01:16:06 -0500

    read args

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,20 @@
 
 int main(int argc, char** argv) {
   klytics::KLytics k_lytics{};
+  std::string      std_out{};
 
-  auto result = k_lytics.get_follower_count();
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      if (strcmp(argv[i], "followers") == 0) {
+        std_out += k_lytics.get_follower_count() + "\n";
+      }
+      if (strcmp(argv[i], "likes") == 0) {
+        std_out += "Likes feature will be implemented soon\n";
+      }
+    }
+  }
 
-  std::cout << "Result is: \n" << result << std::endl;
+  std::cout << std_out << std::endl;
 
   return 0;
 }
