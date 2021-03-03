38604b23a88ecee5301c5f56fa2f3487a907352a
logicp <logicp@logicp.ca>
2021-03-03 16:41:05 -0500

    making our own

--- src/klytics/common/types.hpp
+++ src/klytics/common/types.hpp
@@ -2,10 +2,41 @@
 
 #include <vector>
 #include <string>
-#include <nlohmann/json.hpp>
 
 namespace klytics {
+const static bool JSON_PARSE_NO_THROW{false};
 
+static std::string GetJSONStringValue(nlohmann::json data, std::string key) {
+  if (!data.is_null() && data.contains(key) && !data[key].is_null())
+    return data[key].get<std::string>();
+  return "";
+}
+
+template<typename T>
+static T GetJSONValue(nlohmann::json data, std::string key) {
+  if (!data.is_null() && data.contains(key) && !data[key].is_null())
+    return data[key].get<T>();
+
+  if (std::is_integral<T>::value)
+    return static_cast<T>(0);
+  if (std::is_floating_point<T>::value)
+    return static_cast<T>(0);
+  if constexpr (std::is_same_v<std::string, T>)
+    return "";
+  if constexpr (std::is_same_v<std::vector<std::string>, T>) {
+    return std::vector<std::string>{};
+  }
+
+  throw std::invalid_argument{"Unsupported type"};
+}
+
+static bool GetJSONBoolValue(nlohmann::json data, std::string key) {
+  if (!data.is_null() && data.contains(key) && !data[key].is_null())
+    return data[key].get<bool>();
+  return "";
+}
+// using   GetJSONValue = kjson::GetJSONBoolValue;
+// using   GetJSONValue = kjson::GetJSONBoolValue;
 static std::string ParseMediaURL(const std::string& url)
 {
   auto pos = url.find_first_of('?');
